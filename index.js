2a685a6415df5d39f4bbda1f6da3375a19e72077
Garted <garted93@gmail.com>
2023-04-09 13:48:44 +0300

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+// const storeName = "Garted";
+// const storeDescription = {
+// 	budget: 10000,
+// 	employees:["anna", "piter", "john"],
+// 	products:{
+// 		"mobile": 1000,
+// 		"computer": 5000,
+// 	},
+// 	open: true
+// };
+// console.log (storeDescription);
+
+// alert ("hUA");
+// const result = confirm ("are you here?");
+// console.log (result);
+
+const answer = prompt ("Are you 18 years old?", "18");
+console.log (typeof(answer));
